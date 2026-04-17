a40f5283972f6bb072cddbd0794c0cf3d8d2a714
001234docrazy <chinkf-wp23@student.tarc.edu.my>
2026-04-17 11:17:29 +0800

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -1595,35 +1595,4 @@ elif mode == "💰 Mortgage Calculator":
     "print(f\"\\n✓ Project Complete\")\n",
     "print(\"=\"*120)"
    ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "05eb1179-38b8-4f76-a085-fd0c23d7fbe1",
-   "metadata": {},
-   "outputs": [],
-   "source": []
   }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python [conda env:base] *",
-   "language": "python",
-   "name": "conda-base-py"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
